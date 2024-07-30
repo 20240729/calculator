7d6a60a271b20a0c19760b50c23035e1dc772662
Robbie <robbie331@>
2024-07-30 20:55:42 +0900

    사칙연산 기호에 따른 연산

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,11 +8,25 @@ public class App {
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
 
         System.out.print("사칙연산 기호를 입력하세요. : ");
         char operator = sc.next().charAt(0);
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+
+
+        // 입력받은 양의 정수와 사칙연산 기호를 사용해 연산한 후 결과값 출력하기
+
+        int result = 0;
+        switch(operator){
+            case ('+'): result = num1 + num2; break;
+            case ('-'): result = num1 - num2; break;
+            case ('*'): result = num1 * num2; break;
+            case ('/'): result = num1 / num2; break;
+        }
+        System.out.println("결과: " + result);
 
 
     }
